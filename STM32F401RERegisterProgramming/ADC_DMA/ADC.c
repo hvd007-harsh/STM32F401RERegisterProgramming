fff6bb55d53d919546c1ab72ed64c401afeaa9a7
software_one <software1@neiot.in>
2023-10-23 18:20:28 +0530

    b5

--- STM32F401RERegisterProgramming/ADC_DMA/ADC.c
+++ STM32F401RERegisterProgramming/ADC_DMA/ADC.c
@@ -12,5 +12,21 @@ void ADC_Init(void){
 	7. Set the Respective GPIO PINs in the Analog Mode
 	**************************************************
 	*/
-	
+	//Enabling ADC and GPIO Clock 
+	RCC->APB2ENR |= (1<<8); //Enabling ADC Clock 
+	RCC->AHB1ENR |= (1<<0); //Enabling GPIOA Clock 
+	//2. Set the Prescalar in the Common Control Register 
+	ADC->CCR |= (2 << 16);
+	//3. Setting the Scan Mode and Resolution in the Control Register
+	ADC1->CR1 = (1<<8); //SCAN mode enabled 
+	ADC1->CR2 &=  (~(1<<24));//12 Bit RES
+	//4. Set the Continuous Conversion , EOC, and Data Alignment in Control
+	ADC1->CR2 = (1<<1);
+	ADC1->CR2 |= (1<<10); 
+	ADC1->CR2 &= ~(1<<11);
+	//Enable DMA for ADC 
+	ADC1->CR2 |= (1<<8);
+	//Enable Continuous Request 
+	ADC1->CR2 |= (1<<9); 
+	//5. Setting the sampling time
 }
