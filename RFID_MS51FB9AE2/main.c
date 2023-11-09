18bd5da3b88f15e6b5a3013f771ad582338f2a9e
software_one <software1@neiot.in>
2023-11-09 19:38:33 +0530

    b14

--- RFID_MS51FB9AE2/main.c
+++ RFID_MS51FB9AE2/main.c
@@ -6,18 +6,40 @@
 #include "rfid.h"
 #include "delay.h"
 #include "adc.h"
- 
-unsigned char temp _at_ 0x08;
-unsigned char idata itemp _at_ 0x90;
-unsigned char xdata xtemp _at_ 0x80;
- 
- 
- unsigned char status = '\0';
- float ADCDataIN6, ADCDataIN7;
- unsigned char str[7];
- unsigned char CardAUTH[7]; 
- bit flag = 0;
- 
+#include "eeprom.h"
+
+unsigned char uart_receive_data;
+bit receiveFlag,bufOverFlag;
+
+void Serial_ISR(void) interrupt 4
+{
+    _push_(SFRS);
+  
+    if (RI)
+    {
+        uart0_receive_flag = 1;
+        uart0_receive_data = SBUF;
+			  clr_SCON_RI;                                         // Clear RI (Receive Interrupt).
+    }
+
+    if (TI)
+    {
+        if (!PRINTFG)
+        {
+            TI = 0;
+        }
+    }
+    _pop_(SFRS);
+}
+
+unsigned char status = '\0';
+float ADCDataIN6, ADCDataIN7;
+unsigned char str[7];
+unsigned char CardAUTH[7];
+unsigned char i,j, k;
+unsigned char ArrayData[50],ReceivedData1[5],ReceivedData2[5],ArrRead1[5],ArrRead2[5] ;
+
+bit flag = 0;
 void sw_delay(int ms);
 void SPI_Initial(void);
 
@@ -41,16 +63,67 @@ void main (void)
     MODIFY_HIRC(HIRC_24);
     P06_QUASI_MODE;
     UART_Open(24000000,UART0_Timer3,115200);
-    ENABLE_UART0_PRINTF;
+	  ENABLE_UART0_INTERRUPT;
+	  ENABLE_GLOBAL_INTERRUPT;
+		ENABLE_UART0_PRINTF;
 	  SPI_Initial();
 	  P12 = 1;
 	  P13 = P14 = 0;
 	  sw_delay(3000);
 	  MFRC522_Init();
-	  sw_delay(2000);
+		Read_DATAFLASH_ARRAY(0x38E0,&ArrRead1,5); 
+		Read_DATAFLASH_ARRAY(0x38F0,&ArrRead2,5);
 while(1)
 {
-	  sw_delay(500);
+		if(uart0_receive_flag){
+				i++;
+				ArrayData[i]  = uart0_receive_data;
+				uart0_receive_flag = 0;
+		}
+		if(i >= 20)
+		{
+		  for(j = 0; j <= 20; j++){
+         if((ArrayData[j] == 'C') && (ArrayData[j+1] == 'O') && (ArrayData[j+2] == 'R') && (ArrayData[j+3]=='F') && (ArrayData[j+4]=='#'))
+         {
+					 if(ArrayData[j+5] == '1')
+					 {
+						 ReceivedData1[0] = ArrayData[j+6];
+						 ReceivedData1[1] = ArrayData[j+7];
+						 ReceivedData1[2] = ArrayData[j+8];
+						 ReceivedData1[3] = ArrayData[j+9];
+						 ReceivedData1[4] = ArrayData[j+10];
+					 }
+					 if(ArrayData[j+5] == '2')
+					 {
+						 ReceivedData2[0] = ArrayData[j+6];
+						 ReceivedData2[1] = ArrayData[j+7];
+						 ReceivedData2[2] = ArrayData[j+8];
+						 ReceivedData2[3] = ArrayData[j+9];
+						 ReceivedData2[4] = ArrayData[j+10];
+					 }
+					 if( ArrayData[j+5] == '1'){
+					   Write_DATAFLASH_ARRAY(0x38E0,ReceivedData1,5); //write 5 bytes 					
+					 }
+					 if(ArrayData[j+5] == '2'){
+					   Write_DATAFLASH_ARRAY(0x38F0,ReceivedData2,5);
+					 }
+					 i = 0;
+					 memset(ArrayData ,'\0',sizeof(ArrayData));
+					 Read_DATAFLASH_ARRAY(0x38E0,&ArrRead1,5); 
+					 Read_DATAFLASH_ARRAY(0x38F0,&ArrRead2,5);
+					 
+					 for( k = 0; k <= 5; k++)
+					 {
+						 printf("%x",ArrRead1[i]);
+					 }
+					 for(k = 0; k <= 5; k++)
+					 {
+						 printf("%x",ArrRead2[i]); 
+			     } 
+				 }
+			}
+		}
+		
 	  status = MFRC522_Request(PICC_REQIDL, str);	//MFRC522_Request(0x26, str)
 		status = MFRC522_Anticoll(str);	//Take a collision, look up 5 bytes
 	  ADCDataIN6 = ADC_CH6_Read();
@@ -61,14 +134,14 @@ while(1)
 	  ADCDataIN7 = ADCDataIN7 * 0.00078125;
 	  if (status == MI_OK)
 		{
-		   if(((str[0] == 0XD3 ) &&
-						(str[1] == 0x2) &&
-						(str[2] == 0x3) &&
-						(str[3] == 0xAD))||
-						((str[0] == 0X43 ) &&
-						(str[1] == 0x35) &&
-						(str[2] == 0x05) &&
-						(str[3] == 0xAD)))
+		   if(((str[0] == ArrRead1[0] ) &&
+						(str[1] == ArrRead1[1]) &&
+						(str[2] == ArrRead1[2]) &&
+						(str[3] == ArrRead1[3]))||
+						((str[0] == ArrRead2[0]) &&
+						(str[1] == ArrRead2[1]) &&
+						(str[2] == ArrRead2[2]) &&
+						(str[3] == ArrRead2[3])))
 			 {
 				 if(flag == 0){
 						CardFiller(&CardAUTH,&str);
@@ -115,9 +188,9 @@ while(1)
 				 }
 			  }else{
 					printf("UNAUTHORIZED CARD \r\n");
-				}
-			 }
+			}
 		}
+	}
 }
  
 }
